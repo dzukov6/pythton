3891b9a24bd58d8de03d147f0a2c378891e4ba33
dzukov6 <daniel.zukov@hkhk.edu.ee>
2024-01-10 09:53:37 +0200

    iseseisev töö
    Panzerhund

---
+++ #TÜ iseseisevtöö.py
@@ -0,0 +1,52 @@
+#TÜ iseseisevtöö
+#Daniel Zukov
+#10.01.2024
+
+import random
+import datetime
+#from datetime import * 
+
+
+#thavli juurde
+
+fail = open("nimekiri.txt", encoding="UTF-8" )
+ 
+print(datetime.datetime.now().day) 
+nr = 1
+for rida in fail:
+    if nr==p:
+         print(f"mangitav muusika on: {rida}")
+    nr+=1
+
+
+
+
+
+# failinimi =  input("palun sisestage failinimi: ")
+# fail = open("edm.txt", encoding="UTF-8")
+# nr = 1
+# for rida in fail:
+#     print("f{nr}. {rida}", end="")
+#     nr+=1
+
+# fail.seek(0)
+# nr = 1
+# jrk = int(input("\nvalige jarjekorra nr: "))
+# for rida in fail:
+#     if nr==jrk:
+#         print(f"mangitav muusika on: {rida}")
+#         nr+=1
+
+
+
+
+
+#fail = open("konto.txt", encoding="UTF-8")
+#for rida in fail:
+#    if float(rida) > 0:
+#        print(float(rida))
+
+#fail.close()
+
+
+
